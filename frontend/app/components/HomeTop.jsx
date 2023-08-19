34d13da2f0ab885a85efacc5fe5ac8c5af985a90
Rutuvik Patil <you@example.com>
2023-08-19 18:03:08 +0530

    Navbar and Top Section of Landing Page

---
+++ frontend/app/components/HomeTop.jsx
@@ -0,0 +1,28 @@
+'use client'
+
+import { Box, Heading, Image, Text, VStack } from "@chakra-ui/react"
+import { Head } from "next/document"
+
+export default function HomeTop() {
+  return (
+    <>
+    <Box backgroundColor={'purple.100'} h={'550px'} w={'80%'} m={'auto'} mt={'20px'} borderRadius={'18px'}>
+      <Box display={'flex'} justifyContent={'space-around'} alignItems={'center'}>
+        <VStack w={'25%'} alignItems={'flex-start'}>
+          <Text>Ahead app</Text>
+          <Heading>Master your life by mastering emotions</Heading>
+          <Box mt={'10px'}>
+          <Image src="https://www.ahead-app.com/images/app-store.svg"/>
+          </Box>
+        </VStack>
+        <Image src="https://storage.googleapis.com/web-api-media-uploads/media/Image_1_8d38f94793/Image_1_8d38f94793.svg"/>
+      </Box>
+    </Box>
+    <Box display={'flex'} w={'80%'} m={'auto'} mt={'40px'} justifyContent={'space-between'}>
+      <Heading size={'lg'}>EQ beats IQ</Heading>
+      <Text w={'25%'} color={'gray'}>People with high Emotional Intelligence(EQ) live more fulfilled lives. They tend to be happier and have healthier relationships.</Text>
+      <Text w={'25%'} color={'gray'}>They are more successfull in their pursuits and make for inspiring leaders. According to science, they earn $29K a year.</Text>
+    </Box>
+    </>
+  )
+}
